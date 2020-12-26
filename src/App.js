5c98aba13a562ab0819a50b865d3e60502852ff0
Leandro <leandro.feitoza8018@gmail.com>
2020-12-26 19:19:42 -0300

    Adicionado as rotas do App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import EditMovie from './pages/EditMovie';
+import NewMovie from './pages/NewMovie';
+import MovieDetails from './pages/MovieDetails';
+import MovieList from './pages/MovieList';
+import NotFound from './pages/NotFound';
+
+
+
 
 function App() {
   return (
-    <div>Movie Card Library crud</div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/movies/:id/edit">
+          <EditMovie />
+        </Route>
+        <Route exact path="/movies/new">
+          <NewMovie />
+        </Route>
+        <Route exact path="movies/:id">
+          <MovieDetails />
+        </Route>
+        <Route exact path="/">
+          <MovieList />
+        </Route>
+        <Route exact path="*">
+          <NotFound />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
