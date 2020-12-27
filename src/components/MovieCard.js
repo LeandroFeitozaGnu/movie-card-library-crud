7a8c5612cb7be9718d49a95a686e879fa7227737
Leandro <leandro.feitoza8018@gmail.com>
2020-12-26 21:16:20 -0300

    Criado componente moviecard.

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import { Link, Route } from 'react-router-dom';
+import MovieDetails from '../pages/MovieDetails';
 
 class MovieCard extends React.Component {
   render() {
+    const {movie} = this.props
+    const {
+      title,
+      subtitle,
+      storyline,
+      imagePath,
+      id,
+    } = movie;
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <img src={imagePath} alt={`Poster do filme ${title}`} />
+        <h4> {title} </h4>
+        <h5> {subtitle} </h5>
+        <span> {storyline} </span>
+        <Link to={`/movies/${id}`}>VER DETALHES</Link>
+        <Route path="movies/:id"
+          render={(props) => <MovieDetails {...props}/>}
+        />
       </div>
     );
   }
